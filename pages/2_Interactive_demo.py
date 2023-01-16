dd798e8886b10bae22abc396913792c8204a1e0d
Anna Laws <a.laws2@exeter.ac.uk>
2023-01-16 15:57:29 +0000

    Basic working demo - select team, get bar charts

--- pages/2_Interactive_demo.py
+++ pages/2_Interactive_demo.py
@@ -9,15 +9,25 @@ done in functions stored in files named container_(something).py
 """
 # ----- Imports -----
 import streamlit as st
+import pandas as pd
+
+# Add an extra bit to the path if we need to.
+# Try importing something as though we're running this from the same
+# directory as the landing page.
+try:
+    from utilities_pathway.fixed_params import page_setup
+except ModuleNotFoundError:
+    # If the import fails, add the landing page directory to path.
+    # Assume that the script is being run from the directory above
+    # the landing page directory, which is called
+    # streamlit_pathway_improvement.
+    import sys
+    sys.path.append('./streamlit_pathway_improvement/')
 
 # Custom functions:
-from utilities.fixed_params import page_setup
-from utilities.inputs import \
+from utilities_pathway.fixed_params import page_setup
+from utilities_pathway.inputs import \
     write_text_from_file
-# Containers:
-import utilities.container_inputs
-import utilities.container_results
-import utilities.container_details
 
 
 # ###########################
@@ -26,44 +36,93 @@ import utilities.container_details
 page_setup()
 
 # Title:
-st.markdown('# Interactive demo')
-# Draw a blue information box:
-st.info(
-    ':information_source: ' +
-    'For acronym reference, see the introduction page.'
-    )
-# Intro text:
-write_text_from_file('pages/text_for_pages/2_Intro_for_demo.txt',
-                     head_lines_to_skip=2)
+st.markdown('# Pathway improvement')
 
 
 # ###########################
 # ########## SETUP ##########
 # ###########################
-st.header('Setup')
-st.subheader('Inputs')
 
-# Draw the input selection boxes in this function:
-animals_df, animal, feature, row_value = utilities.container_inputs.main()
+df = pd.read_csv(
+    'utilities_data/scenario_results.csv',
+    # index_col=index_col,
+    header='infer'
+    )
+
+# Baseline_good_outcomes_(median)
+# Baseline_good_outcomes_per_1000_patients_(low_5%)
+# Baseline_good_outcomes_per_1000_patients_(high_95%)
+# Baseline_good_outcomes_per_1000_patients_(mean)
+# Baseline_good_outcomes_per_1000_patients_(stdev)
+# Baseline_good_outcomes_per_1000_patients_(95ci)
+# Percent_Thrombolysis_(median%)
+# Percent_Thrombolysis_(low_5%)
+# Percent_Thrombolysis_(high_95%)
+# Percent_Thrombolysis_(mean)
+# Percent_Thrombolysis_(stdev)
+# Percent_Thrombolysis_(95ci)
+# Additional_good_outcomes_per_1000_patients_(median)
+# Additional_good_outcomes_per_1000_patients_(low_5%)
+# Additional_good_outcomes_per_1000_patients_(high_95%)
+# Additional_good_outcomes_per_1000_patients_(mean)
+# Additional_good_outcomes_per_1000_patients_(stdev)
+# Additional_good_outcomes_per_1000_patients_(95ci)
+# Onset_to_needle_(mean)
+# calibration
+# scenario
+# stroke_team
+
+# Pull out the list of stroke teams:
+stroke_team_list = sorted(set(df['stroke_team'].to_numpy()))
+
+# Select a team
+team_input = st.selectbox(
+    'Pick a team',
+    stroke_team_list
+    )
+
+# Pick out just the data for the chosen team:
+df_here = df[df['stroke_team'] == team_input]
+
 
 
 # ###########################
 # ######### RESULTS #########
 # ###########################
-st.header('Results')
-# Draw a plot in this function:
-utilities.container_results.main(row_value, animal, feature)
+
+# Make a bar chart of the mean values:
+import plotly.graph_objects as go
+
+cols_bar = st.columns(2)
+with cols_bar[0]:
+    # Percentage thrombolysis use:
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df_here['scenario'],
+        y=df_here['Percent_Thrombolysis_(mean)']
+    ))
+
+    fig.update_xaxes(title='Scenario')
+    fig.update_yaxes(title='Percent Thrombolysis (mean)')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+with cols_bar[1]:
+    # Additional good outcomes 
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df_here['scenario'],
+        y=df_here['Additional_good_outcomes_per_1000_patients_(mean)'],
+        marker=dict(color='red')
+    ))
+
+    fig.update_xaxes(title='Scenario')
+    fig.update_yaxes(title='Additional_good_outcomes_per_1000_patients_(mean)')
+
+    st.plotly_chart(fig, use_container_width=True)
 
 
-# ###########################
-# ######### DETAILS #########
-# ###########################
-st.write('-'*50)
-st.header('Details of the calculation')
-st.write('The following bits detail the calculation.')
-
-with st.expander('Some details'):
-    # Draw the equation in this function:
-    utilities.container_details.main(animal, feature, row_value)
+st.write('Full data for this team: ')
+st.write(df_here)
 
 # ----- The end! -----
