96e8742dca59c2de0ec89acd9df88734d6738a72
Anna Laws <a.laws2@exeter.ac.uk>
2023-01-17 17:07:00 +0000

    New bar chart with changing values for each scenario. Work-in-progress. Code messy!

--- pages/2_Interactive_demo.py
+++ pages/2_Interactive_demo.py
@@ -12,6 +12,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import plotly.graph_objects as go
+# For importing colours:
 import plotly.express as px
 import matplotlib
 
@@ -56,11 +58,27 @@ def main():
     # ########## SETUP ##########
     # ###########################
 
-    df = pd.read_csv(
+
+    df_full = pd.read_csv(
         dir + 'data_pathway/scenario_results.csv',
         # index_col=index_col,
         header='infer'
         )
+    
+    # Pull out the list of stroke teams:
+    stroke_team_list = sorted(set(df_full['stroke_team'].to_numpy()))
+
+    scenarios = [
+        'base',
+        'speed',
+        'onset',
+        'benchmark',
+        'speed_onset',
+        'speed_benchmark',
+        'onset_benchmark',
+        'speed_onset_benchmark',
+        # 'same_patient_characteristics'
+    ]
 
     # Baseline_good_outcomes_(median)
     # Baseline_good_outcomes_per_1000_patients_(low_5%)
@@ -85,20 +103,117 @@ def main():
     # scenario
     # stroke_team
 
-    scenarios = [
-        'base',
-        'speed',
-        'onset',
-        'benchmark',
-        'speed_onset',
-        'speed_benchmark',
-        'onset_benchmark',
-        'speed_onset_benchmark',
-        # 'same_patient_characteristics'
+    # Reduce this dataframe to just the important features:
+    key_features = [
+        'Baseline_good_outcomes_per_1000_patients_(mean)',
+        'Percent_Thrombolysis_(mean)',
+        'Additional_good_outcomes_per_1000_patients_(mean)',
+        # 'Onset_to_needle_(mean)',
+        # 'calibration',
+        'scenario',
+        'stroke_team'
     ]
+    df = df_full[key_features].copy()
 
-    # Pull out the list of stroke teams:
-    stroke_team_list = sorted(set(df['stroke_team'].to_numpy()))
+    # Remove "same patient characteristics" scenario:
+    df = df[df['scenario'].str.contains('same_patient_characteristics') == False]
+
+    # Add some new columns:
+    diff_perc_thromb = [0]*len(stroke_team_list)
+    diff_additional_good = [0]*len(stroke_team_list)
+    for scenario in scenarios[1:]:
+        df_here = df[df['scenario'] == scenario]
+        diff_perc_thromb_here = (
+            df_here['Percent_Thrombolysis_(mean)'].values -
+            df['Percent_Thrombolysis_(mean)'][df['scenario'] == 'base'].values
+            )
+
+        diff_additional_good_here = (
+            df_here['Additional_good_outcomes_per_1000_patients_(mean)'].values -
+            df['Additional_good_outcomes_per_1000_patients_(mean)'][df['scenario'] == 'base'].values
+            )
+        # diff_perc_thromb.append(diff_perc_thromb_here)
+        diff_perc_thromb = np.concatenate((diff_perc_thromb, diff_perc_thromb_here))
+        diff_additional_good = np.concatenate((diff_additional_good, diff_additional_good_here))
+    df['Percent_Thrombolysis_(mean)_diff'] = diff_perc_thromb
+    df['Additional_good_outcomes_per_1000_patients_(mean)_diff'] = diff_additional_good
+
+
+    # User inputs:
+    cols_radio = st.columns(2)
+    with cols_radio[0]:
+        scenario_for_rank = st.radio(
+            'Sort values by this:',
+            options=['base', 'chosen difference scenario']
+            )
+    with cols_radio[1]:
+        scenario = st.radio(
+            'Show difference due to:',
+            options=scenarios
+            )
+    if scenario_for_rank != 'base':
+        scenario_for_rank = scenario
+
+    # Add sorted base rank:
+    # Make a new "Index" column that ranks the teams alphabetically
+    # (or default input order). Each team gets the same index value
+    # across all of thes different scenarios.
+    index_original_col = np.tile(np.arange(len(stroke_team_list)), len(scenarios))
+    df['Index'] = index_original_col
+    # Sort the values by the mean percent of thrombolysis column
+    # in the "base" scenario.
+    # Extract the values for just this scenario:
+    df_base = df[df['scenario'] == scenario_for_rank].copy()
+    # Sort with largest value at the top:
+    df_sorted_rank = df_base.sort_values('Percent_Thrombolysis_(mean)', ascending=False)
+    # Add rank, largest value = 1, smallest = 132 (or number of teams).
+    df_sorted_rank['Rank'] = np.arange(1, len(stroke_team_list) + 1)
+    # Now re-sort back to the starting order...
+    df_sorted_rank = df_sorted_rank.sort_values('Index')
+    # ... and these are the ranks for all of the teams:
+    df_sorted_rank_index = df_sorted_rank['Rank'].values
+    # Copy this multiple times, one set for each scenario:
+    sorted_rank_col = np.tile(df_sorted_rank_index, len(scenarios))
+    # Add this column to the main data frame:
+    df['Rank_(base_percent_thrombolysis_mean)'] = sorted_rank_col
+
+    # Percentage thrombolysis use:
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df['Rank_(base_percent_thrombolysis_mean)'][df['scenario'] == 'base'],
+        y=df['Percent_Thrombolysis_(mean)'][df['scenario'] == 'base'],
+        name='base'
+    ))
+
+    if scenario != 'base':
+        y_diffs = (
+            df['Percent_Thrombolysis_(mean)'][df['scenario'] == scenario].values -
+            df['Percent_Thrombolysis_(mean)'][df['scenario'] == 'base'].values
+        )
+        
+        leg_str = scenario.replace('_', '_<br>')
+        fig.add_trace(go.Bar(
+            x=df['Rank_(base_percent_thrombolysis_mean)'][df['scenario'] == scenario],
+            y=y_diffs,
+            name=f'difference due to<br>{leg_str}'
+        ))
+
+
+    # Change the bar mode
+    fig.update_layout(barmode='stack')
+
+    fig.update_xaxes(title=f'Rank sorted by {scenario_for_rank}')
+    fig.update_yaxes(title='Percent Thrombolysis (mean)')
+    fig.update_layout(title=f'{scenario}')
+
+    fig.update_yaxes(range=[0, max(df['Percent_Thrombolysis_(mean)'])*1.05])
+
+    # fig.update_yaxes(range=[0, max_percent_thrombolysis_mean*1.05])
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
 
     # Select a team
     highlighted_teams = st.multiselect(
@@ -108,14 +223,11 @@ def main():
     highlighted_colours = px.colors.qualitative.Plotly + list(matplotlib.colors.cnames.values())
 
 
-
-
     # ###########################
     # ######### RESULTS #########
     # ###########################
 
     # Make a bar chart of the mean values:
-    import plotly.graph_objects as go
 
     # Find max y values across all teams.
     max_percent_thrombolysis_mean = max(df['Percent_Thrombolysis_(mean)'])
@@ -131,13 +243,14 @@ def main():
             fig = go.Figure()
             fig.add_trace(go.Bar(
                 x=df_here['scenario'],
-                y=df_here['Percent_Thrombolysis_(mean)']
+                # y=df_here['Percent_Thrombolysis_(mean)'],
+                y=df_here['Percent_Thrombolysis_(mean)_diff'],
             ))
 
             fig.update_xaxes(title='Scenario')
-            fig.update_yaxes(title='Percent Thrombolysis (mean)')
+            fig.update_yaxes(title='DIFF Percent Thrombolysis (mean)')
 
-            fig.update_yaxes(range=[0, max_percent_thrombolysis_mean*1.05])
+            # fig.update_yaxes(range=[0, max_percent_thrombolysis_mean*1.05])
 
             st.plotly_chart(fig, use_container_width=True)
 
@@ -146,14 +259,15 @@ def main():
             fig = go.Figure()
             fig.add_trace(go.Bar(
                 x=df_here['scenario'],
-                y=df_here['Additional_good_outcomes_per_1000_patients_(mean)'],
+                # y=df_here['Additional_good_outcomes_per_1000_patients_(mean)'],
+                y=df_here['Additional_good_outcomes_per_1000_patients_(mean)_diff'],
                 marker=dict(color='red')
             ))
 
             fig.update_xaxes(title='Scenario')
-            fig.update_yaxes(title='Additional good outcomes<br>per 1000 patients<br>(mean)')
+            fig.update_yaxes(title='DIFF Additional good outcomes<br>per 1000 patients<br>(mean)')
 
-            fig.update_yaxes(range=[0, max_additional_good_mean*1.05])
+            # fig.update_yaxes(range=[0, max_additional_good_mean*1.05])
 
             st.plotly_chart(fig, use_container_width=True)
 
@@ -199,11 +313,11 @@ def main():
 
     x_offsets_scatter = find_offsets_for_scatter(len(highlighted_teams))
 
-    med_vals = df['Percent_Thrombolysis_(median%)'].values
+    # med_vals = df['Percent_Thrombolysis_(mean%)'].values
     for x, scenario in enumerate(scenarios):
 
         df_scenario = df[df['scenario'] == scenario]
-        med_vals = df_scenario['Percent_Thrombolysis_(median%)'].values
+        med_vals = df_scenario['Percent_Thrombolysis_(mean)'].values
 
         fig.add_trace(go.Violin(
             y=med_vals,
@@ -234,7 +348,7 @@ def main():
         # Add scatter markers for highlighted teams
         showlegend_scatter = False if x > 0 else True
         for t, team in enumerate(highlighted_teams):
-            y_team = df_scenario['Percent_Thrombolysis_(median%)'][df_scenario['stroke_team'] == team].values
+            y_team = df_scenario['Percent_Thrombolysis_(mean)'][df_scenario['stroke_team'] == team].values
             fig.add_trace(go.Scatter(
                 y=y_team,
                 x=[x+x_offsets_scatter[t]],
@@ -304,8 +418,6 @@ def main():
 
         st.plotly_chart(fig, use_container_width=True)
 
-    st.write('Full data for this team: ')
-    st.write(df_here)
 
     # ----- The end! -----
 
